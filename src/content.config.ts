f4431ea5e69d395b60da27fd48de73281f588ca4
Karthik Sivakumar <karthik29062000@gmail.com>
2026-03-19 22:58:21 +0530

    feat: added testimonial cards

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,18 +1,32 @@
-import { defineCollection } from 'astro:content';
-import { glob } from 'astro/loaders';
-import { z } from 'astro/zod';
+import { defineCollection } from "astro:content";
+import { glob, file } from "astro/loaders";
+import { z } from "astro/zod";
 
 const productsCollection = defineCollection({
-  loader: glob({ base: './src/content/products', pattern: '**/*.mdx' }),
-  schema: ({ image }) =>
-    z.object({
-      slug: z.string(),
-      title: z.string(),
-      price: z.number(),
-      shortDescription: z.string(),
-      description: z.string(),
-      image: image(),
+    loader: glob({ base: "./src/content/products", pattern: "**/*.mdx" }),
+    schema: ({ image }) =>
+        z.object({
+            slug: z.string(),
+            title: z.string(),
+            price: z.number(),
+            shortDescription: z.string(),
+            description: z.string(),
+            image: image(),
+            imageHover: image(),
+        }),
+});
+
+const testimonialsCollection = defineCollection({
+    loader: file("./src/content/testimonials/testimonials.json"),
+    schema: z.object({
+        id: z.string(),
+        review: z.string(),
+        name: z.string(),
+        shop: z.string(),
     }),
 });
 
-export const collections = { products: productsCollection };
+export const collections = {
+    products: productsCollection,
+    testimonials: testimonialsCollection,
+};
